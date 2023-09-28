5fcbf7e1aae30bac614425a64af729dae8c94385
jorgezoto <tito.jorge12@gmail.com>
2023-09-28 20:38:30 -0300

    clase 38 lista

---
+++ src/controllers/moviesController.js
@@ -0,0 +1,93 @@
+const db = require("../database/models");
+const sequelize = db.sequelize;
+const moment = require("moment");
+
+//Otra forma de llamar a los modelos
+const Movies = db.Movie;
+
+const moviesController = {
+  list: (req, res) => {
+    db.Movie.findAll().then((movies) => {
+      res.render("moviesList.ejs", { movies });
+    });
+  },
+  detail: (req, res) => {
+    db.Movie.findByPk(req.params.id).then((movie) => {
+      res.render("moviesDetail.ejs", { movie });
+    });
+  },
+  new: (req, res) => {
+    db.Movie.findAll({
+      order: [["release_date", "DESC"]],
+      limit: 5,
+    }).then((movies) => {
+      res.render("newestMovies", { movies });
+    });
+  },
+  recomended: (req, res) => {
+    db.Movie.findAll({
+      where: {
+        rating: { [db.Sequelize.Op.gte]: 8 },
+      },
+      order: [["rating", "DESC"]],
+    }).then((movies) => {
+      res.render("recommendedMovies.ejs", { movies });
+    });
+  }, //Aqui debemos modificar y completar lo necesario para trabajar con el CRUD
+  add: function (req, res) {
+    res.render("moviesAdd");
+  },
+  create: function (req, res) {
+    db.Movie.create({
+      title: req.body.title,
+      rating: req.body.rating,
+      length: req.body.length,
+      awards: req.body.awards,
+      release_date: req.body.release_date,
+    }).then((newMovie) => {
+      //console.log(newMovie);
+      //res.redirect("/movies/detail");
+      res.redirect("/movies");
+    });
+  },
+  edit: function (req, res) {
+    db.Movie.findByPk(req.params.id).then((movie) => {
+      //se envia el moment para formatear fecha
+      res.render("moviesEdit.ejs", { Movie: movie, moment });
+    });
+  },
+  update: function (req, res) {
+    db.Movie.update(
+      {
+        title: req.body.title,
+        rating: req.body.rating,
+        length: req.body.length,
+        awards: req.body.awards,
+        release_date: req.body.release_date,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    ).then(() => {
+      res.redirect("/movies");
+    });
+  },
+  delete: function (req, res) {
+    db.Movie.findByPk(req.params.id).then((movie) => {
+      res.render("moviesDelete.ejs", { Movie: movie });
+    });
+  },
+  destroy: function (req, res) {
+    db.Movie.destroy({
+      where: {
+        id: req.params.id,
+      },
+    }).then(() => {
+      res.redirect("/movies");
+    });
+  },
+};
+
+module.exports = moviesController;
